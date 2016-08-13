d655e4322899036737cb5213b429aaca2f1cc4ae
msingh <msingh@iitmlogin5.tropmet.res.in>
2016-08-14 00:31:30 +0530

    added mechanize code

---
+++ mechanize_codes/testing_trmm.py
@@ -0,0 +1,35 @@
+import mechanize
+import cookielib
+
+br = mechanize.Browser()
+# Cookie Jar
+cj = cookielib.LWPCookieJar()
+br.set_cookiejar(cj)
+
+# Browser options
+br.set_handle_equiv(True)
+br.set_handle_gzip(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+
+# Follows refresh 0 but not hangs on refresh > 0
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+# Want debugging messages?
+#br.set_debug_http(True)
+#br.set_debug_redirects(True)
+#br.set_debug_responses(True)
+
+# User-Agent (this is cheating, ok?)
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+br.set_handle_robots(False)
+br.open("https://storm.pps.eosdis.nasa.gov/storm/data/Service.jsp?serviceName=RestrictedOrder")
+#br.select_form(nr=0)
+#br.form['q'] = 'manmeet'
+#br.submit()
+#br.response.read()
+
+
+for f in br.forms():
+    print f
